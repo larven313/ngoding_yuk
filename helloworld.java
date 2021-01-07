3b758599b3f86fbc871247773e70e46b3ee16f03
Zan03 <77084786+Zan03@users.noreply.github.com>
2021-01-07 10:45:36 +0700

    Update helloworld.java
    tambahan untuk tutorial sukma

--- helloworld.java
+++ helloworld.java
@@ -3,4 +3,8 @@ public static void main(){
 System.out.println("Hello World");
 }
   System.out.println("Sukmax tutorial");
+  System.out.println("tutorial nafas");
+  System.out.println("tutorial mandi");
+  System.out.println("tutorial tidur");
+  
 }
